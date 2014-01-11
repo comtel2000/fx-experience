855c6a9b941892fb75188f0f63c89d117cbcaef0
comtel <comtel2000@googlemail.com>
2014-01-11 21:19:05 +0100

    8.0.9-SNAPSHOT

--- fx-onscreen-keyboard/src/main/java/org/comtel/javafx/control/KeyboardPane.java
+++ fx-onscreen-keyboard/src/main/java/org/comtel/javafx/control/KeyboardPane.java
@@ -465,14 +465,14 @@ public class KeyboardPane extends Region implements StandardKeyCode, EventHandle
 				colIdx++;
 				rowWidth += cc.getPrefWidth();
 			}
-			logger.info("row[{}] - {}", rowIdx, rowWidth);
+			logger.trace("row[{}] - {}", rowIdx, rowWidth);
 			colPane.getRowConstraints().add(rc);
 			// colPane.setGridLinesVisible(true);
 			rPane.add(colPane, 0, rowIdx);
 			rowIdx++;
 		}
 
-		logger.debug("-----end pane-----");
+		logger.trace("-----end pane-----");
 		return rPane;
 	}
 
