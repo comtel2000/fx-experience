ae811506f64885a6b0411a7bf9e068c822da0d24
comtel <comtel2000@googlemail.com>
2014-01-19 16:36:54 +0100

    upd jre7 pom

---
+++ fx-onscreen-keyboard/launch_jre7.bat
@@ -0,0 +1,4 @@
+::  -lang <locale>          setting app language (de,en,ru)
+::  -layout <path>          path to custom layout xml
+::  -pos <x,y>              initial keyboard position
+start javaw -version:1.7.0 -cp "fx-onscreen-keyboard-1.0.9-SNAPSHOT-jar-with-dependencies.jar;%PROGRAMFILES%\Java\jre7\lib\jfxrt.jar" org.comtel.javafx.AwtStandAloneApp -lang en -pos 200,200
